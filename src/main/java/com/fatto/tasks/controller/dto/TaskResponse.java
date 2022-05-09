5e8d9e4e65ef6ce82f383520baa96c156a86833a
matheuscrok <matheusprogfut@outlook.com>
2022-05-08 21:30:32 -0300

    inserir tarefa

--- src/main/java/com/fatto/tasks/controller/dto/TaskResponse.java
+++ src/main/java/com/fatto/tasks/controller/dto/TaskResponse.java
@@ -1,5 +1,14 @@
 package com.fatto.tasks.controller.dto;
 
+import java.sql.Date;
+
+import lombok.Data;
+
+@Data
 public class TaskResponse {
-    
+    private Long id;
+	private String nome;
+	private Double custo;
+	private Date dataLimite;
+	private int ordemApresentacao;
 }
